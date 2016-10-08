baf733611a1259e0d78e6cd87985cc2a77d8a240
jamst <jamstjamst@163.com>
2016-10-08 09:19:10 +0800

    添加附件

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,23 @@
 class Article < ApplicationRecord
+
+  has_many :attachments, as: :attachment
+
+  def attachment_paths
+    self.attachments.map{|attachment|  attachment.attachment_path }
+  end
+
+  def attachment_path
+    if self.attachments.present?
+      return self.attachments.to_arry.last.attachment_path
+    end
+    nil
+  end
+
+  def attachment_name
+    if self.attachments.present?
+      return self.attachments.to_arry.last.name
+    end  
+    nil
+  end
+
 end
